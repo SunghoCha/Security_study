ea0462f9929b5ca294aa62b317e59c945d99d590
SunghoCha <tjdgh3725@gmail.com>
2024-08-24 23:15:38 +0900

    security - 커스텀 로그아웃 방식(get방식, 비추천)

--- src/main/java/com/sh/security/controller/LoginController.java
+++ src/main/java/com/sh/security/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.sh.security.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,4 +21,15 @@ public class LoginController {
     public String signup() {
         return "login/signup";
     }
+
+    @GetMapping("/logout") // get 방식으로도 가능하지만 기존의 post방식 하는걸 추천..
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login";
+    }
+
 }
